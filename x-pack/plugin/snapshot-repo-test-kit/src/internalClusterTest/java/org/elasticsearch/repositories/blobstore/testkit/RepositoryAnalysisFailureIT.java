3c4aa72019f31c0b378af9779912a0596d514d54
Yang Wang <yang.wang@elastic.co>
2024-01-18 13:31:00 +1100

    logging

--- x-pack/plugin/snapshot-repo-test-kit/src/internalClusterTest/java/org/elasticsearch/repositories/blobstore/testkit/RepositoryAnalysisFailureIT.java
+++ x-pack/plugin/snapshot-repo-test-kit/src/internalClusterTest/java/org/elasticsearch/repositories/blobstore/testkit/RepositoryAnalysisFailureIT.java
@@ -164,6 +164,7 @@ public class RepositoryAnalysisFailureIT extends AbstractSnapshotIntegTestCase {
                 if (actualContents != null && countDown.countDown()) {
                     // CRC32 should always detect a single bit flip
                     disruptedContents[Math.toIntExact(position + randomLongBetween(0, length - 1))] ^= (byte) (1 << between(0, 7));
+                    logger.info("--> disrupted contents: [{}], actual contents [{}]", disruptedContents, actualContents);
                 }
                 return disruptedContents;
             }
