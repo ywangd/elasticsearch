3291a620dcb63984aa6f2c02e39022d82cdba0d6
Yang Wang <yang.wang@elastic.co>
2024-04-26 16:56:14 +1000

    Flush before deleting index in cleanup
    To avoid commit to be held by VBCC

--- test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/elasticsearch/test/InternalTestCluster.java
@@ -1361,6 +1361,8 @@ public final class InternalTestCluster extends TestCluster {
                     for (IndexShard indexShard : indexService) {
                         try {
                             Engine engine = IndexShardTestCase.getEngine(indexShard);
+                            // TODO: flush so that no reference is held by VBCC
+                            engine.flush(false, true);
                             if (engine instanceof InternalEngine) {
                                 assertFalse(
                                     indexShard.routingEntry().toString() + " has unreleased snapshotted index commits",
